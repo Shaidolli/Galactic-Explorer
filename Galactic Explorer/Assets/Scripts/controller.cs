b5c1559fb518c36fc8a340a5866875e6167505f6
makscorsa <maks.corsa@gmail.com>
2023-04-18 22:10:55 +0300

    пофиксил управление
    (скопировал код с ютуба, ноя его вроде как понял)

--- Galactic Explorer/Assets/Scripts/controller.cs
+++ Galactic Explorer/Assets/Scripts/controller.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class controller : MonoBehaviour
 {
-
     public CharacterController controller1;
 
-    public float speed = 10;
-    public float gravity = 5;
-    
+    public float speed = 12f;
+    public float gravity = -9f;
+    public float jumpHeight = 3f;
 
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+
         float x = Input.GetAxis("Horizontal");
-        float z  = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * -x + transform.forward * -z;
-        controller1.Move(move*speed*Time.deltaTime);
+
+        controller1.Move(move * speed * Time.deltaTime);
+
+
+
+
+        controller1.Move(velocity * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+
     }
 }
+
