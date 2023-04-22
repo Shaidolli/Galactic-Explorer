cd2f4cc278b5a2d3f37b5e0a3a3d82994437e004
makscorsa <maks.corsa@gmail.com>
2023-04-22 11:00:07 +0300

    недоротеишн

---
+++ Galactic Explorer/Assets/Scripts/rotblaster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotblaster : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 raznica;
+
+    void Start()
+    {
+        raznica = transform.position - player.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.position + raznica;
+    }
+}
