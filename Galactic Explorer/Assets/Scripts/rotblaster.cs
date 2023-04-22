483ba5a22f885e06937d4ccdcb8d3997cddb048a
makscorsa <maks.corsa@gmail.com>
2023-04-22 13:27:08 +0300

    новій скрипт на ротеішн
    хз как но я его сделал и фиг знает как он работает

--- Galactic Explorer/Assets/Scripts/rotblaster.cs
+++ Galactic Explorer/Assets/Scripts/rotblaster.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class rotblaster : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 raznica;
+    public Quaternion rot;
+   // public Transform player;
+    //public Vector3 raznica;
 
     void Start()
     {
-        raznica = transform.position - player.position;
+       // raznica = transform.position - player.position;
     }
 
     void Update()
     {
-        transform.position = player.position + raznica;
+        //transform.position = player.position + raznica;
+
+        this.transform.rotation = rot;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            rot.y = 0f;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            rot.y = 0f;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rot.y = 0f;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            rot.y = 0f;
+        }
     }
 }
